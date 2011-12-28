2814be961f58c0c3f3ea59551dd6da3b72c263ed
Schinken <schinken@hackerspace-bamberg.de>
2011-12-28 20:27:23 +0100

    Added caching in 500ms intervals

--- debug/proxy.php
+++ debug/proxy.php
@@ -6,7 +6,24 @@
  * Time: 9:50 AM
  */
 
-$ch = curl_init( 'http://api.openbeacon.net/get/brucon.json' );
+
+$apcExists = false;
+
+if( function_exists('apc_add') ) {
+    $apcExists = true;
+}
+
+if( $apcExists ) {
+    $cache = microtime(true)*1000;
+    $cache = sprintf("%.1f", round($cache/100*2)/2 );
+
+    if(  ( $result = apc_fetch( $cache ) ) !== false ) {
+        echo $result;
+        exit;
+    }
+}
+
+$ch = curl_init( 'http://176.99.39.100/tracking.json' );
 
 curl_setopt( $ch, CURLOPT_USERAGENT, "28c3 r0ketr5ck debug proxy" );
 curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1);
@@ -15,4 +32,10 @@ curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 5);
 curl_setopt( $ch, CURLOPT_ENCODING, 'gzip,deflate');
 
 header('Content-type: application/json');
-echo curl_exec( $ch );
+$result = curl_exec( $ch );
+
+echo $result;
+
+if( $apcExists ) {
+    apc_add( $cache, $result, 3 );
+}
