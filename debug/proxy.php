d5be51215976fe7185a11c74adebf0723979f771
Schinken <schinken@hackerspace-bamberg.de>
2011-12-28 14:58:12 +0100

    Removing debug stuff (not needed anymore)

--- debug/proxy.php
+++ debug/proxy.php
@@ -6,16 +6,6 @@
  * Time: 9:50 AM
  */
 
-if( isset($_REQUEST['example']) ) {
-
-    $rnd = mt_rand(1,30);
-
-    header('Content-type: application/json');
-    echo file_get_contents( sprintf('exampledata/data%d.json', $rnd ) );
-
-    exit;
-}
-
 $ch = curl_init( 'http://api.openbeacon.net/get/brucon.json' );
 
 curl_setopt( $ch, CURLOPT_USERAGENT, "28c3 r0ketr5ck debug proxy" );
