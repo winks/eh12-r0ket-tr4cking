e3dda1fd6a67fded36f15193a067d1ec522386c7
Schinken <schinken@hackerspace-bamberg.de>
2011-12-29 15:23:17 +0100

    Adding layer toogle colors

--- static/classes/floor.js
+++ static/classes/floor.js
@@ -1,22 +1,35 @@
 function Floor( name ){
-    var that=this;
-    this.invalid=false;
-    this.display=true;
-    this.name=name;
-    this.count=0;
-    var $div=$( '<div/>' );
+
+    var that        =   this;
+    this.invalid    =   false;
+    this.display    =   true;
+    this.name       =   name;
+    this.count      =   0;
+
+    var $div = $( '<div/>' );
+
     $( "#floormenu" )
             .append( $div )
-            .append( '<br/>' );
-    
+            .append( '<br/>' )
+            .addClass('floor-active');
+
     $div.click(function(){
-        that.display= !that.display;
+        that.display = !that.display;
+
+        $div.removeClass('floor-active floor-inactive');
+        if( that.display ) {
+            $div.addClass('floor-active');
+        } else {
+            $div.addClass('floor-inactive');
+        }
+
     });
     
-    this.updateText=function() {
-        $div.html( "<h2>Floor " + this.name + " " + this.count + "r0kets</h2>" );
+    this.updateText = function() {
+        $div.html( "Floor " + this.name + " " + this.count + "r0kets" );
     };
-    this.destructor=function() {
+
+    this.destructor = function() {
         $div.remove();
     };
     
