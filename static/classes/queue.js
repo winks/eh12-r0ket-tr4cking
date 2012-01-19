8d791a7db5fa155835b8b4a714549677f0dfd861
zaphod1984 <zaphod84@gmx.de>
2012-01-19 12:29:34 +0100

    refactored queue code, using prototyped method declaration

--- static/classes/queue.js
+++ static/classes/queue.js
@@ -6,35 +6,34 @@
 
 
 function Queue( n ) {
-
-    var queue  = [];
-    var length = n || 3;
-
-    /**
-     * Adds an Item to the end of the queue,
-     * if there are enough items in it, remove older ones
-     * @param Item
-     */
-    this.push = function( Item ) {
-
-        if( queue.length >= length ) {
-            queue.shift();
-        }
-
-        queue.push( Item );
-    }
-
-    /**
-     * Returns the whole queue
-     */
-    this.getQueue = function() {
-        return queue;
-    }
-
-    /**
-     * Returns the topmost Item
-     */
-    this.getTop = function() {
-        return queue[ queue.length-1 ];
-    }
+    this.queue  = [];
+    this.length = n || 3;
 }
+
+/**
+ * Returns the topmost Item
+ */
+Queue.prototype.getTop = function() {
+    return this.queue[ this.queue.length-1 ];
+}
+
+/**
+ * Returns the whole queue
+ */
+Queue.prototype.getQueue = function() {
+    return this.queue;
+}
+
+/**
+ * Adds an Item to the end of the queue,
+ * if there are enough items in it, remove older ones
+ * @param Item
+ */
+Queue.prototype.push = function( Item ) {
+
+    if( this.queue.length >= this.length ) {
+        this.queue.shift();
+    }
+
+    this.queue.push( Item );
+};
